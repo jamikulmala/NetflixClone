0059a43dad40ae4417ad56d928b157ca5872ebdd
JamiKulmala <jami.kulmala@tuni.fi>
2023-05-06 18:18:58 +0300

    searchbar

--- moviebrowser/src/components/searchbar.jsx
+++ moviebrowser/src/components/searchbar.jsx
@@ -1,56 +1,37 @@
 import { Box, IconButton, InputAdornment, TextField } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
 import { useNavigate } from "react-router";
-import { useSpring, animated } from "react-spring";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 export const SearchBar = (props) => {
-
   const navigate = useNavigate();
+  const [searchInput, setSearchInput] = useState('');
+  props.updatePage("search");
+  const handleSearchInputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
 
-  useEffect(() => {
-    props.updatePage("search")
-  });
-
-    const [searchInput, setSearchInput] = useState('');
-    const [isFocused, setIsFocused] = useState(false);
-
-    const handleSearchInputChange = (event) => {
-        setSearchInput(event.target.value);
-      };
-
-    const handleInputFocus = () => {
-        setIsFocused(true);
-      };
-    
-    const handleInputBlur = () => {
-        setIsFocused(false);
-      };
-    
-    const searchInputAnim = useSpring({
-        width: isFocused ? "300px" : "200px",
-      });
-
-      return (
-        <Box>
-        <animated.div style={searchInputAnim}>
-          <TextField
-            value={searchInput}
-            style={{ backgroundColor: 'black' }}
-            onChange={handleSearchInputChange}
-            onFocus={handleInputFocus}
-            onBlur={handleInputBlur}
-            placeholder="Search for title" 
-            InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SearchIcon />
-              </InputAdornment>
-            )}}/>
-            <IconButton onClick={() => navigate("/home")} size="large"><CloseIcon /></IconButton>
-          </animated.div>
-          </Box>
-
-      )
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#141414', padding: '40px 0' }}>
+      <TextField
+        value={searchInput}
+        sx={{ color: 'white', width: '70%', maxWidth: '800px' }}
+        onChange={handleSearchInputChange}
+        placeholder="Search for a title"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon sx={{ color: 'white' }} />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Box sx={{ marginLeft: '20px' }}>
+        <IconButton sx={{ color: "white" }} size="large"><ArrowForwardIcon /></IconButton>
+        <IconButton sx={{ color: "white" }} onClick={() => navigate("/home")} size="large"><CloseIcon /></IconButton>
+      </Box>
+    </Box>
+  )
 }
